c63e9cff3832a772bb380a3cbeef41e702665c75
confident1 <47142559+confident1@users.noreply.github.com>
2019-02-17 18:29:12 +0100

    Update example.js

--- examples/example-01/example.js
+++ examples/example-01/example.js
@@ -1,15 +1,16 @@
 var el = document.querySelector('#triangle')
 
+
 // From purple to green
 function animate1() {
   dynamics.animate(el, {
-    rotateZ: 180,
-    scale: 1.5,
-    borderBottomColor: '#43F086'
+    rotateZ: 360,
+    scale: 2.5,
+  /*  borderBottomColor: '#4FDF086'*/
   }, {
-    type: dynamics.spring,
+    /*type: dynamics.spring,*/
     friction: 400,
-    duration: 1300,
+    duration: 1000,
     complete: animate2
   })
 }
@@ -17,16 +18,16 @@ function animate1() {
 // From green to purple
 function animate2() {
   dynamics.animate(el, {
-    rotateZ: 360,
-    scale: 1,
-    borderBottomColor: '#CA2F6F'
+    rotateZ: 180,
+    scale: 2.5,
+  /*  borderBottomColor: '#CA35F6F'*/
   }, {
-    type: dynamics.spring,
+  /*  type: dynamics.spring,*/
     frequency: 600,
     friction: 400,
-    duration: 1800,
+    duration: 1000,
     anticipationSize: 350,
     anticipationStrength: 400,
-    complete: animate1
+   complete: animate1
   })
 }
