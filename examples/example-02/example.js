611ea60739891e89bed546c5120dbd766dee1111
antonforsell <47349360+antonforsell@users.noreply.github.com>
2019-02-18 19:48:58 +0100

    example 2 update

--- examples/example-02/example.js
+++ examples/example-02/example.js
@@ -1,3 +1,4 @@
+//Assigning variables to dom elements
 var el = document.querySelector('path')
 var textEl = document.querySelector('span')
 var pathOver = el.getAttribute('data-over-path')
@@ -5,41 +6,73 @@ var pathOut = el.getAttribute('d')
 
 // Over animation
 function animateOver() {
+  //Animates an element to the properties with the animation options.
+  //"el" is a dom element (button svg path) passed as an argument.
+  //dynamics.animate(el, properties, options)
   dynamics.animate(el, {
+    //bends the button path
     d: pathOver,
-    fill: "#007EFF"
+    //color for the over animation
+    fill: "#32CD32",
   }, {
-    type: dynamics.spring,
+    //animation type
+    type: dynamics.bounce,
+    //Realistic animation properties
+    elasticity: 500,
+    friction: 400,
+    duration: 3000,
+    //completion callback
     complete: animateOut
   })
 
+  //Animates an element to the properties with the animation options.
+  //textEL is a dom element passed as an argument.
+  //dynamics.animate(el, properties, options)
   dynamics.animate(textEl, {
-    scale: 1.06
+    //size increase of buttontext
+    scale: 2,
+    //rotation animation (text) (12*)
+    rotateZ: 12
   }, {
-    type: dynamics.spring
+    //animation type
+    type: dynamics.forceWithGravity
   })
 }
 
+
+
 // Out animation
 function animateOut() {
+  //Animates an element to the properties with the animation options.
+  //"el" is a dom element passed as an argument.
+  //dynamics.animate(el, properties, options)
   dynamics.animate(el, {
     d: pathOut,
-    fill: "#0bf"
+    fill: "#20B2AA",
   }, {
-    type: dynamics.easeInOut,
-    friction: 100
+    //animation type
+    type: dynamics.bounce,
+    //Realistic animation properties
+    elasticity: 1500,
+    friction: 200
   })
 
+  //Animates an element to the properties with the animation options.
+  //textEL is a dom element passed as an argument.
+  //dynamics.animate(el, properties, options)
   dynamics.animate(textEl, {
-    scale: 1
+    //return button text size to initial size
+    scale: 1,
   }, {
-    type: dynamics.easeInOut,
-    friction: 100,
+    //animation type
+    type: dynamics.forceWithGravity,
+    //completion callback with a function
     complete: function() {
-      dynamics.setTimeout(animateOver, 500)
+      //setTimeout replaces requestAnimationFrame beacuse it can run in a background tab in the browser
+      dynamics.setTimeout(animateOver)
     }
   })
 }
 
-// Start
+// (call animateOver) Start the animation loop
 animateOver()
