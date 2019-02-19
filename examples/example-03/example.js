ab1de0209627a638cfe7b31efa85fc655ce3427b
Link5a <richardmodecir@gmail.com>
2019-02-19 01:29:37 +0100

    example 3 succesfully expanded
    Done?

--- examples/example-03/example.js
+++ examples/example-03/example.js
@@ -1,20 +1,20 @@
 var dots = document.querySelectorAll('.dot')
-var colors = ['#007EFF', '#FF3700', '#92FF00']
+var colors = ['#168BF2', '#00FFFF', '#55D968', '#F2C12E', '#F23054']
 
 // Start the 3 dot animations with different delays
 function animateDots() {
   for(var i=0; i<dots.length; i++) {
     dynamics.animate(dots[i], {
-      translateY: -70,
+      translateY: -150,
       backgroundColor: colors[i]
     }, {
       type: dynamics.forceWithGravity,
-      bounciness: 800,
-      elasticity: 200,
-      duration: 2000,
-      delay: i * 450
+      bounciness: 200,
+      elasticity: 100,
+      duration: 3000,
+      delay: i * 150
     })
   }
 
-  dynamics.setTimeout(animateDots, 2500)
+  dynamics.setTimeout(animateDots, 3500)
 }
