76f582f06e3bf025c35c35a95da7682d1c199b6e
Patrik Svensson <a.patrik.svensson@gmail.com>
2019-02-19 14:21:04 +0100

    Added some horizontal movement and changed the color to black.

--- examples/example-05/example.js
+++ examples/example-05/example.js
@@ -1,11 +1,12 @@
 var pin = document.querySelector('.pin')
 var svg = document.querySelector('.pin svg')
+var moveAmount = -350
 
 function horizontalBounce() {
   // The bounce animation will return to the original state
   // In this case, it will go from 0deg to -45deg to 0deg
   dynamics.animate(pin, {
-    rotateZ: -45
+    rotateZ: -45,
   }, {
     type: dynamics.bounce,
     duration: 1800,
@@ -16,11 +17,11 @@ function horizontalBounce() {
 function verticalBounce() {
   // We animate the two elements (svg, pin) independently
   dynamics.animate(svg, {
-    scaleY: 0.8
+    scaleY: 0.1
   }, {
     type: dynamics.bounce,
     duration: 800,
-    bounciness: 0
+    bounciness: 3
   })
 
   // Use the delay option to delay your animations
@@ -40,6 +41,18 @@ function verticalBounce() {
     duration: 800,
     bounciness: 600,
     delay: 650,
+    complete: horizontalMove
+  })
+}
+
+function horizontalMove() {
+  dynamics.animate(pin, {
+    translateX: moveAmount
+  }, {
+    type: dynamics.bounce,
+    duration: 1500,
+    delay: 500,
     complete: horizontalBounce
   })
+  moveAmount = -moveAmount;
 }
